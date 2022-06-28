ba308a58473e7b19c3251836505772c0eca236cc
DarioPazmino <dariopb1497@gmail.com>
2022-06-28 13:11:45 -0500

    Input component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
-import FeedbackData from './data/FeedbackData';
+import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
 
   const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete this feedback?')) {
+    if (window.confirm("Are you sure you want to delete this feedback?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
-  }
+  };
 
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
